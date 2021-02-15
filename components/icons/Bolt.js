949777b10e263f52f1fb4a183ad66ccdbf76a276
Monkey <florentheyraud.pro@gmail.com>
2021-02-15 19:36:18 +0100

    feat(index): add icons

---
+++ components/icons/Bolt.js
@@ -0,0 +1,25 @@
+import React from "react";
+export default function Bolt() {
+  return (
+    <>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        dataReactroot=""
+      >
+        <path
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          strokeMiterlimit="10"
+          strokeWidth="1"
+          stroke="#221b38"
+          fill="none"
+          d="M16 2L6 13.5H10L8 22L18 11.5H14L16 2Z"
+        ></path>
+      </svg>
+    </>
+  );
+}
